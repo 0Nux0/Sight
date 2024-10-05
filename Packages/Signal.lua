25808952948b727c465c4ac84b6a17945b1619e3
Nux <126259402+0Nux0@users.noreply.github.com>
2024-10-05 18:11:48 -0300

    Init

---
+++ Packages/Signal.lua
@@ -0,0 +1,15 @@
+export type ScriptSignal<T...> = {
+	IsActive: (self: ScriptSignal<T...>) -> boolean,
+	Fire: (self: ScriptSignal<T...>, T...) -> (),
+	Connect: (self: ScriptSignal<T...>, callback: (T...) -> ()) -> ScriptConnection,
+	Once: (self: ScriptSignal<T...>, callback: (T...) -> ()) -> ScriptConnection,
+	DisconnectAll: (self: ScriptSignal<T...>) -> (),
+	Destroy: (self: ScriptSignal<T...>) -> (),
+	Wait: (self: ScriptSignal<T...>) -> T...,
+}
+export type ScriptConnection = {
+	Disconnect: (self: ScriptConnection) -> (),
+	Connected: boolean,
+}
+
+return require(script.Parent._Index["lucasmzreal_fastsignal@10.3.1"]["fastsignal"])
